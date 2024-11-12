6875ceab2c99c2834680a5a32fe886e47cffbde3
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-12 12:39:38 +0900

    chapter2セクションを終了

---
+++ chapter2-SFSC/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava {
+  // Passing in Arguments
+  public static void main(String[] args) {
+    System.out.println("Hello " + args[0]);
+  }
+}
---
+++ chapter2-SFSC/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+  // single-file source-code program
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
