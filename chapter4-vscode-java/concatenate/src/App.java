df63e7470f76ad332aebd9be84a8b051a73fc455
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-12 13:48:52 +0900

    vscodeを使用してプロジェクトを作成しました

---
+++ chapter4-vscode-java/concatenate/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+  // Created using command pallete of vscode
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
