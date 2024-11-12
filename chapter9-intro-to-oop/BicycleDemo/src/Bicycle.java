c7d3727411bd6fdc6b8b53c87441e8f27655318f
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-12 14:53:55 +0900

    Bicycle クラスの実装

--- chapter9-intro-to-oop/BicycleDemo/src/Bicycle.java
+++ chapter9-intro-to-oop/BicycleDemo/src/Bicycle.java
@@ -1,27 +1,27 @@
 public class Bicycle {
-  int cadence = 0;
-  int speed = 0;
-  int gear = 1;
+   int cadence = 0;
+    int speed = 0;
+    int gear = 1;
 
-  void changeCadence(int newValue) {
-       cadence = newValue;
-  }
+    void changeCadence(int newValue) {
+         cadence = newValue;
+    }
 
-  void changeGear(int newValue) {
-       gear = newValue;
-  }
+    void changeGear(int newValue) {
+         gear = newValue;
+    }
 
-  void speedUp(int increment) {
-       speed = speed + increment;
-  }
+    void speedUp(int increment) {
+         speed = speed + increment;
+    }
 
-  void applyBrakes(int decrement) {
-       speed = speed - decrement;
-  }
+    void applyBrakes(int decrement) {
+         speed = speed - decrement;
+    }
 
-  void printStates() {
-       System.out.println("cadence:" +
-           cadence + " speed:" +
-           speed + " gear:" + gear);
-  }
+    void printStates() {
+         System.out.println("cadence:" +
+             cadence + " speed:" +
+             speed + " gear:" + gear);
+    }
 }
