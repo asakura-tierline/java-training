4c40d81869c67935308f849b0ccef5d58c141ccf
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-13 13:14:46 +0900

    Creating and Using Objects demo

---
+++ chapter11-objects-classes/CreatingClasses/src/Point.java
@@ -0,0 +1,10 @@
+public class Point {
+  // Purpose: Define a class that represents a point in 2D space.
+  public int x = 0;
+  public int y = 0;
+  // a constructor!
+  public Point(int a, int b) {
+  x = a;
+  y = b;
+  }
+}
